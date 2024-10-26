079967160aa9221e0d1d2e11325cb156a2dcccdb
xddaso <francodelucchi18@gmail.com>
2024-10-26 02:20:37 +0200

    cambios para fedora

--- install_powerline.sh
+++ install_powerline.sh
@@ -2,10 +2,10 @@
 set -eux pipefail
 
 # Install Powerline for VIM.
-sudo apt install -y python3-pip
+sudo dnf install -y python3-pip
 pip3 install --user powerline-status
 sudo cp configs/.vimrc ~/.vimrc
-sudo apt install -y fonts-powerline
+sudo dnf install -y fonts-powerline
 
 # Install Patched Font
 mkdir ~/.fonts
--- install_terminal.sh
+++ install_terminal.sh
@@ -2,5 +2,5 @@
 set -eux pipefail
 
 # Install ZSH
-sudo apt install -y git-core zsh curl
+sudo dnf install -y git-core zsh curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
